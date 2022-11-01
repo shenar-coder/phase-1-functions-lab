d3c8a19650dcd5062f1dd42e541aeb34415799ae
shinar3@gmail.com <110408797+shenar-coder@users.noreply.github.com>
2022-11-01 14:42:25 -0400

    commit tree a20b70b52c0454e2010f6fa194cd17fbeaf98c46

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks (destination) {
+  if (destination >= 42) {
+      return destination - 42;
+  }
+  if (destination < 42) {
+      return 42 - destination;
+  }
+}
+
+function distanceFromHqInFeet (destination) {
+  let blocks = distanceFromHqInBlocks (destination);
+  return blocks * 264;
+}
+
+function distanceTravelledInFeet (start, end) {
+  if (end >= start) {
+      let blocksTravelled = end - start;
+      return blocksTravelled * 264;
+  }
+  if (end < start) {
+      let blocksTravelled = start - end;
+      return blocksTravelled * 264;
+  }
+}
+
+function calculatesFarePrice (start, end) {
+  let distance = distanceTravelledInFeet (start, end);
+  if (distance <= 400) {
+      return 0;
+  }
+  if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+  }
+  if (distance > 2000 && distance <= 2500) {
+      return 25;
+  }
+  if (distance > 2500) {
+      return 'cannot travel that far';
+  }
+}
